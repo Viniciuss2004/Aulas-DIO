2bf664a7bd2089c10b7588a8eb746f343ed23289
Viniciuss2004 <viandcustodio@gmail.com>
2024-07-26 12:38:51 -0300

    Atualização dos tipos primitivos

---
+++ PJ1 (Repositório GitHub)/TiposVariaveis/src/TiposVariaveisprim.java
@@ -0,0 +1,15 @@
+public class TiposVariaveisprim {
+    public static void main(String[] args) {
+
+        // Tipos primitivos
+
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+        boolean vf = true; // Verdadeiro ou falso
+        char letra = 'A'; // Cararcterers;
+    }
+}
