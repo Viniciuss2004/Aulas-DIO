9778daa6e050b76cf07a22bbd4ffecbb834d9b19
Viniciuss2004 <viandcustodio@gmail.com>
2024-07-27 15:32:27 -0300

    Ajeitando pasta dos projetos

---
+++ Projetos/PJ1/cod/src/PJ1.java
@@ -0,0 +1,10 @@
+package cod.src;
+
+public class PJ1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Nenhum projeto por enquanto");
+
+    }
+}
--- Projetos/PJ1/src/PJ1.java
+++
@@ -1,5 +0,0 @@
-public class PJ1 {
-    public static void main(String[] args) {
-        System.out.println("Nenhum projeto por enquanto!");
-    }
-}
