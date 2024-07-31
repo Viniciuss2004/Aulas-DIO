87e0354abb49499587bb200a1f3642a73a9142b1
Viniciuss2004 <viandcustodio@gmail.com>
2024-07-30 23:04:56 -0300

    Exercicios de Arrays e Loops

--- Exemplos e Exercicios/Exercicio/ControleDeFluxo/cod/src/Data.java
+++ Exemplos e Exercicios/Exercicio/ControleDeFluxo/cod/src/Data.java
@@ -6,7 +6,7 @@ public class Data {
 
     public static void main(String[] args) {
 
-        System.out.println("Digite o dia atual: ");
+        /*System.out.println("Digite o dia atual: ");
         Scanner sc = new Scanner(System.in);
         int dia = sc.nextInt();
 
@@ -16,7 +16,8 @@ public class Data {
         System.out.println("Digite o ano atual: ");
         int ano = sc.nextInt();
 
-        System.out.println("0" + dia + "/0" + mes + "/" + ano);
+        System.out.println("0" + dia + "/0" + mes + "/" + ano);*/
+
 
     }
 }
